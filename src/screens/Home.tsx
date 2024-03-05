ba7adde97e6601056fe2cc6b0610af0155e3a7d4
Emily Gorelik <egorelik@jahnelgroup.com>
2024-03-05 13:31:08 -0500

    add icons

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,12 @@
+import HouseIcon from "../assets/house";
+
 function Home() {
   return (
-    <div className="bg-primary text-center p-4">
-      <h1 className="text-xl font-bold text-white">JG Planning Poker</h1>
+    <div className="bg-primary p-4 h-16 flex items-center">
+      <HouseIcon className="fill-white h-full" />
+      <h1 className="text-xl font-bold text-white mx-auto">
+        JG Planning Poker
+      </h1>
     </div>
   );
 }
