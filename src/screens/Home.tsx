f81375bc8f5cd36910f73d8102162a434bcad93e
Emily Gorelik <egorelik@jahnelgroup.com>
2024-03-05 12:55:15 -0500

    remove starter code and add daisy ui

---
+++ src/screens/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="bg-primary text-center p-4">
+      <h1 className="text-xl font-bold text-white">JG Planning Poker</h1>
+    </div>
+  );
+}
+
+export default Home;
