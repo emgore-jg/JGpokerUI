87e1761de7bfe8317f1f0d25a57d3f45a17041cb
Emily Gorelik <egorelik@jahnelgroup.com>
2024-03-06 15:39:54 -0500

    little bit of header styling

--- src/screens/AppHeader.tsx
+++ src/screens/AppHeader.tsx
@@ -4,7 +4,7 @@ function AppHeader() {
   return (
     <div className="sticky top-0 z-10 flex h-16 items-center bg-accent p-4">
       <HouseIcon className="h-full fill-accent-content" />
-      <h1 className="mx-auto text-xl font-bold text-accent-content">
+      <h1 className="mx-auto text-4xl font-semibold text-accent-content">
         JG Planning Poker
       </h1>
     </div>
